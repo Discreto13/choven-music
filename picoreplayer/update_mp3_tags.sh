912415c9f928f3f915340311ffac03fe9e0c2a25
Yehor Shashko <yegor.shashko@gmail.com>
2025-05-06 16:59:50 +0300

    Fix for sh instead of bash
    Signed-off-by: Yehor Shashko <yegor.shashko@gmail.com>

--- picoreplayer/update_mp3_tags.sh
+++ picoreplayer/update_mp3_tags.sh
@@ -9,34 +9,34 @@ INPUT_PATH="$1"
 
 # Формуємо список файлів
 if [ -d "$INPUT_PATH" ]; then
-  files=("$INPUT_PATH"/*.mp3)
+  file_list=$(find "$INPUT_PATH" -type f -name '*.mp3')
 elif [ -f "$INPUT_PATH" ]; then
-  files=("$INPUT_PATH")
+  file_list="$INPUT_PATH"
 else
   echo "❌ Помилка: $INPUT_PATH не знайдено"
   exit 1
 fi
 
-total=${#files[@]}
+total=$(echo "$file_list" | wc -l)
 count=0
 
-for file in "${files[@]}"; do
+echo "$file_list" | while read file; do
   count=$((count + 1))
   percent=$((count * 100 / total))
   echo "$percent% [$count/$total] Обробляю: $file"
 
-  # Перевіряємо тип файлу, перекодовуємо за необхідності
   file_type=$(ffprobe -v error -show_entries format=format_name -of default=nw=1:nk=1 "$file")
-  if [[ "$file_type" != *"mp3"* ]]; then
-      echo "🔄 Файл не MP3, перекодовую: $file"
-      tmp_fixed="${file%.mp3}-fixed.mp3"
-      if ffmpeg -y -i "$file" -codec:a libmp3lame -qscale:a 2 "$tmp_fixed" &> /dev/null; then
-          mv "$tmp_fixed" "$file"
-          echo "✅ Перекодування завершено: $file"
-      else
-          echo "❌ Помилка перекодування: $file"
-          continue
-      fi
+  echo "$file_type" | grep -q "mp3"
+  if [ $? -ne 0 ]; then
+    echo "🔄 Файл не MP3, перекодовую: $file"
+    tmp_fixed="${file%.mp3}-fixed.mp3"
+    if ffmpeg -y -i "$file" -codec:a libmp3lame -qscale:a 2 "$tmp_fixed" >/dev/null 2>&1; then
+      mv "$tmp_fixed" "$file"
+      echo "✅ Перекодування завершено: $file"
+    else
+      echo "❌ Помилка перекодування: $file"
+      continue
+    fi
   fi
 
   filename=$(basename "$file" .mp3)
@@ -48,19 +48,17 @@ for file in "${files[@]}"; do
     continue
   fi
 
-  # Коригування тегів (метаданих)
   tmp_file="${file%.mp3}-tmp.mp3"
   if ffmpeg -y -i "$file" \
     -map_metadata -1 \
     -metadata artist="$artist" \
     -metadata title="$title" \
-    -c copy "$tmp_file" &> /dev/null; then
-
-	mv "$tmp_file" "$file"
+    -c copy "$tmp_file" >/dev/null 2>&1; then
+    mv "$tmp_file" "$file"
     echo "✅ Змінено: Artist='$artist', Title='$title'"
   else
     echo "❌ ПОМИЛКА: Не вдалося змінити теги для $file"
-	[ -f "$tmp_file" ] && rm "$tmp_file"
+    [ -f "$tmp_file" ] && rm "$tmp_file"
   fi
 done
 
